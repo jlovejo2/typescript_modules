7521ee8bb70c1d46756d34ab8589e6008bdb2af1
James Lovejoy <james.lovejoy2@gmail.com>
2021-03-03 11:46:46 -0600

    used different methods for importing in b.ts

--- b.ts
+++ b.ts
@@ -1,3 +1,19 @@
-import { aString } from './a';
+import { aString as theString, bString } from './a';
+import * as strings from './a';
 
-console.log(aString);
+console.log(strings.aString);
+
+import { reverse, isOdd } from './utils';
+
+console.log(reverse('test'), isOdd(1))
+
+
+// import test = require('./a');
+// console.log(test);
+
+if (Math.random() >= 0.5) {
+  import('./a').then(a => console.log('The message is', a.aString))
+}
+
+import * as config from './config.json';
+console.log(config.userSetting1);
